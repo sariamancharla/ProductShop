1fbcbeaa8f2ef12557b534303119b8ee354c1842
sariamancharla <93614345+sariamancharla@users.noreply.github.com>
2022-12-22 22:13:58 +0100

    Create a product class and controller,database file with product list object. Modified the index to display the links to Product of the Month,List of Products,About Us

---
+++ ProductShop/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductShop.Models;
+using System.Collections.Generic;
+
+namespace ProductShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Detail(string slugLink)
+        {
+            Product product = Database.GetProduct(slugLink);
+            return View(product);
+        }
+
+        public IActionResult List()
+        {
+            List<Product> products = Database.GetProducts();
+            return View(products);
+        }
+    }
+}
---
+++ ProductShop/Models/Database.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace ProductShop.Models
+{
+    public class Database
+    {
+        public static List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product{ProductId=1, Name="Product 1", Price=(decimal)499.00 },
+                new Product{ProductId=2, Name="Product Of The Month", Price=(decimal)599.00 },
+                new Product{ProductId=3, Name="Product 3", Price=(decimal)599.00 },
+                new Product{ProductId=4, Name="Product 4", Price=(decimal)599.00 },
+                new Product{ProductId=5, Name="Product 5", Price=(decimal)599.00 },
+                new Product{ProductId=6, Name="Product 6", Price=(decimal)599.00 },
+                new Product{ProductId=7, Name="Product 7", Price=(decimal)599.00 },
+                new Product{ProductId=8, Name="Product 8", Price=(decimal)599.00 },
+                new Product{ProductId=9, Name="Product 9", Price=(decimal)599.00 }
+             };
+            return products;
+        }
+
+        public static Product GetProduct(string slug)
+        {
+            List<Product> product=Database.GetProducts();
+            foreach (Product p in product)
+            {
+                if (p.Slug==slug)
+                {
+                    return p;
+                }
+            }
+            return null;
+        }
+        
+    }
+}
